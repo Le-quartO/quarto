70a63efcdb826d13c1d2a53f5d3e48176f61336c
Augustoni <60670025+Augustoni@users.noreply.github.com>
2020-04-16 22:35:56 +0200

    ghtfy

--- IA1.py
+++ IA1.py
@@ -20,26 +20,20 @@ def IA(case,e,piecev,cord_piece,cord_case,g,p,c,cselection):
             if case2[a][b]== "OOOO":
                 #Mode de jeu gagnant
                 case2[a][b]=piecev[i]
-                test2(case2,r)
-                print(a,b,"ah")
-                if test2(case2,r)==1:
-                    print ("on sait jamais")
+                test2(case2,r,a,b)
+                print("ah")
+                case2[a][b]="OOOO"
+                print (test2(case2,r,a,b))
+                if test2(case2,r,a,b)==1:
                     g=r
                     caseg=1
-                case2[a][b]="OOOO"
 
-
-
-    if caseg==1:
+    if caseg!=0:
         cord_piece[i]=cord_case[g]
         case[g//4][g%4]=piecev[i]
         p[i]=0
         c[g]=1
-
-        print("et ouais mon coco tu t'es fait battre par un programme fait par une terminale")
-        return
     #placement aléatoire
-
     else:
         while caseg!="OOOO":
             print("alea")
@@ -52,26 +46,10 @@ def IA(case,e,piecev,cord_piece,cord_case,g,p,c,cselection):
         c[ca]=1
 
     #sélection de pièce
-    print("selection")
-
-    for z in range (16):
-        while p[e]!=2:
-            e=random.randint(0,15)
-        for r in range (16):
-
-            a=r//4
-            b=r%4
-            if case2[a][b]== "OOOO":
-                #Mode de jeu gagnant
-                case2[a][b]=piecev[e]
-                print(a,b,"ah")
-                print ("on sait jamais")
-                if test2(case2,r)==0:
-                    cord_piece[e]=cselection
-                case2[a][b]="OOOO"
-
-            #mettre ici les coordonnées de la case ou poser les piéces sélectionnées
-
+    while p[i]==0:
+        i=random.randint(0,15)
+        #mettre ici les coordonnées de la case ou poser les piéces sélectionnées
+    cord_piece[i]=cselection
 
 
 
--- fonctions_test.py
+++ fonctions_test.py
@@ -5,26 +5,26 @@ def test(case):
 
 
             if case[x][0][i]==case[x][1][i]and case [x][1][i]==case[x][2][i]and case[x][2][i]==case[x][3][i] and  case[x][3][i]!="O" :
-                print ("bien jouÃƒÂ©")
+                print ("bien jouÃ©")
 
     for y in range(4):
         for i in range(4):
 
 
             if case [0][y][i]==case [1][y][i]and case [1][y][i]==case[2][y][i]and case [2][y][i]==case [3][y][i] and case[3][y][i]!="O" :
-                print ("bien jouÃƒÂ©")
+                print ("bien jouÃ©")
 
 
     for i in range(4):
         if case[0][0][i]==case [1][1][i]and case [1][1][i]==case [2][2][i]and case [2][2][i]==case [3][3][i] and case[3][3][i]!="O" :
-            print ("bien jouÃƒÂ©")
+            print ("bien jouÃ©")
 
     for i in range(4):
         if case[0][3][i]==case [1][2][i]and case [1][2][i]==case [2][1][i] and case [2][1][i]==case [3][0][i] and case[3][0][i]!="O" :
-            print ("bien jouÃƒÂ©")
+            print ("bien jouÃ©")
 
 
-def test2(case2,r):
+def test2(case2,r,a,b):
     print ("essai",r)
     for x in range(4):
         for i in range(4):
@@ -40,6 +40,7 @@ def test2(case2,r):
                 print("1")
                 return 1
 
+
     for i in range(4):
         if case2[0][0][i]==case2 [1][1][i]and case2 [1][1][i]==case2 [2][2][i]and case2 [2][2][i]==case2 [3][3][i] and case2[3][3][i]!="O" :
             print("1")
@@ -49,7 +50,5 @@ def test2(case2,r):
     for i in range(4):
         if case2[0][3][i]==case2 [1][2][i]and case2 [1][2][i]==case2 [2][1][i] and case2 [2][1][i]==case2 [3][0][i] and case2[3][0][i]!="O" :
             return 1
-
-    return 0
 
 
